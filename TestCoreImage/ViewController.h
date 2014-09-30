dcd0273e4eda3576819e0abe323fc731eefae562
Boyko Andrey <flanker@flymac.local>
2014-09-30 13:56:43 +0300

    Initial Commit

---
+++ TestCoreImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCoreImage
+//
+//  Created by Boyko Andrey on 9/30/14.
+//  Copyright (c) 2014 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestCoreImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestCoreImage
+//
+//  Created by Boyko Andrey on 9/30/14.
+//  Copyright (c) 2014 None. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
